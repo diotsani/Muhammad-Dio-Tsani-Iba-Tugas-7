b627a1248231d84b21a9ba18409b40907c80209b
Muhammad Dio Tsani Iba - 24 <dio.tsani123@gmail.com>
2022-08-29 15:42:07 +0800

    delete comment

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private List<AudioSource> _bgmAudioSource;
-    //[SerializeField] private AudioSource _bgmAudioClip;
     public bool isAudioEnabled = false;
      private static AudioManager _instance;
 
     public static AudioManager Instance { get { return _instance; } }
     private void Awake() 
     {
-        //_bgmAudioClip = Resources.Load<AudioSource>("BGM/BGM");
+        _bgmAudioSource = Resources.LoadAll<AudioSource>("Prefabs").ToList();
+
         if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
@@ -22,40 +22,14 @@ public class AudioManager : MonoBehaviour
             DontDestroyOnLoad(this);
         }
     }
-
-    private void Start()
-    {
-        //randomBgm = Random.Range(1, 4);
-        //for (int i = 0; i < randomBgm; i++)
-        //{
-        //    int amountBgm = i;
-        //    AudioSource init = Instantiate(Resources.Load("Prefabs/BGM"+amountBgm, typeof(AudioSource)), transform) as AudioSource;
-        //    _bgmAudioClip.Add(init);
-            
-        //}
-        _bgmAudioSource = Resources.LoadAll<AudioSource>("Prefabs").ToList();
-    }
     public void PlayBgm()
     {
         //_bgmAudioSource[Random.Range(0, _bgmAudioSource.Count)].Play();
 
-        //_bgmAudioClip = Resources.Load<AudioSource>("BGM" + randomBgm) as AudioSource;
         int randomIndex = Random.Range(0, _bgmAudioSource.Count);
-        //_bgmAudioSource[Random.Range(0, _bgmAudioSource.Count)].Play();
         AudioSource randomBgm = Instantiate(_bgmAudioSource[randomIndex], transform);
         randomBgm.Play();
-        //_bgmAudioSource.Clear();
+        _bgmAudioSource.Clear();
         Resources.UnloadUnusedAssets();
-
-        //foreach (AudioSource item in _bgmAudioSource)
-        //{
-        //    Resources.UnloadAsset(item);
-        //}
-        
-
-        //for (int i = 0; i < _bgmAudioClip.Count; i++)
-        //{
-        //    Resources.UnloadUnusedAssets();
-        //}
     }
 }
